8ddd50ced8cb2b2b7f065a192aa6dc234d8b38e6
Nishant <pnishant0824@gmail.com>
2025-06-15 23:09:31 +0530

    setups are done for both server and client side

--- server/index.js
+++ server/index.js
@@ -2,4 +2,25 @@ import express from 'express';
 import bodyparser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import connectDB from './dbconfig.js';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+dotenv.config();
+
+const app=express();
+connectDB();
+app.use(express.json({limit:'30mb'}));
+app.use(express.urlencoded({limit:'30mb',extended:true}));
+
+
+
+app.get('/',(req,res)=>{
+    res.send('API is working');
+});
+app.use('/posts',postRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
 
